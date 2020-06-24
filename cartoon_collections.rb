ad0c79e867c590c30eceeb30539c1b1ba6af2bdf
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-24 21:38:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? {|word| word.length >= 4 }
+  planeteer_calls.any? {|word| word.length > 4 }
 end
 
 def find_valid_calls(planeteer_calls)
