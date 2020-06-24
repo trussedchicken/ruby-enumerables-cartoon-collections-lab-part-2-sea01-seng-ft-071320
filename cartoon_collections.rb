82c69b3aebdded0f063b6617b00bcc0cf310ebc4
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-24 21:40:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? {|word| word.length > 4 }
+  planeteer_calls.any? { |word| word.length > 4 }
 end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-   planeteer_calls.select 
+   planeteer_calls.select { |call| }
 
 end
