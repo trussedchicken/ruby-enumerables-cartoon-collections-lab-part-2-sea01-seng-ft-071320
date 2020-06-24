65a5463efb24165c5f50cf5a558105122c59bfec
Jeremy Winn <jeremyjwinn@gmail.com>
2020-06-24 21:41:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-   planeteer_calls.select { |call| }
+   planeteer_calls.select { |call| call == valid_calls}
 
 end
